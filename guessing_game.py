6e5b7f51c5a86f7db05ac9158d7d2eddeb7a8d8d
Matthew Stuhr <36904235+MatthewStuhr@users.noreply.github.com>
2020-05-05 21:26:47 -0700

    Update
    added a if statement to track the best score. added a variable of play_count to keep track of the best score instead of resetting each time there was a new game. added guess_count +=1 under the last elif to properly keep track of the amount of attempts there are after each game reset.

--- guessing_game.py
+++ guessing_game.py
@@ -17,20 +17,26 @@ def start_game():
     *_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*
     """)
     
-    answer = random.randint(1,10)
+    answer = random.randint(1, 10)
     
-    guess_count = 1
+    guess_count = 0
     
     best_score = 0
+    
+    play_count = 1
     
     while True:
         try:
             new_guess = input("Pick a number between 1 and 10:  ")
             new_guess = int(new_guess)
-            if new_guess <1 or new_guess > 10:
+            if new_guess < 1 or new_guess > 10:
                 raise ValueError("Your guess needs to be within the 1-10 range")
         except ValueError as err:
             print("Oh no, we ran into an issue. {}. Please enter a number between 1 and 10!".format(err))
+            continue
+        
+            
+        
         else:
             if new_guess < answer:
                 print("It is higher!")
@@ -39,27 +45,31 @@ def start_game():
                 print("It is lower!")
                 guess_count += 1
             elif new_guess == answer:
+                guess_count += 1
                 print("You got it! It took you {} attempts.".format(guess_count))
-                best_score = guess_count
                 
+                if guess_count < best_score or play_count == 1:
+                    best_score = guess_count
+                    
                 
                     
                 continue_game = input("Would you like to play another round? Y/N : ")
                 if continue_game.lower() == "y":
                     guess_count = 0
-                    answer = random.randint(1,10)
+                    answer = random.randint(1, 10)
+                    play_count += 1
                     print("Your best score is {}!".format(best_score))
-                    start_game()
+                    
                 elif continue_game.lower() == "n":
                     print("Have a great day!")
                     break
-                break
+                
             
             
     
     
 
 
-
-# Kick off the program by calling the start_game function.
-start_game()
+if __name__ == '__main__':
+    # Kick off the program by calling the start_game function.
+    start_game()
